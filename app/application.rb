4191b006812cbc3bef9404d1b6754b9d37d8f5d6
Christia T <christiatidwell@bellsouth.net>
2018-08-24 13:25:34 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    resp.write "Hello, World"
+    resp.finish
+  end
+
+  if num_1==num_2 && num_2==num_3
+    puts "You Win"
+  else
+    puts "You Lose"
+  end
+ 
+end
