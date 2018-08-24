7e7a7c2bf1cb7bdfedf38da7e425f46c6a1b2826
Christia T <christiatidwell@bellsouth.net>
2018-08-24 13:51:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+
+    num_1 = Time.new(1..12)
+    num_2 = Time.new(12..24)
+    num_3 = Time.new(1..24)
+ 
     if num_1==num_2 && num_2==num_3
-      resp.write  "You Win"
+      resp.write  "Good Morning!"
     else
-      resp.write  "You Lose"
+      resp.write  "Good Afternoon!"
     end
      resp.finish
   end
